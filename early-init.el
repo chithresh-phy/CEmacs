50b4a6d76d7d99c8efe0971bff2f9ce17746a0c3
Chithresh <176747366+chithresh-phy@users.noreply.github.com>
2026-01-01 23:37:27 +0530

    disabling some UI elements before loading emacs

--- early-init.el
+++ early-init.el
@@ -1,4 +1,12 @@
-;; Start Emacs maximized
-(push '(fullscreen . maximized) default-frame-alist)
-;; Disabling the built-in Package.el (For elpaca)
+;; Disabling the built-in Package.el (for elpaca)
 (setq package-enable-at-startup nil)
+
+;; UI
+(tool-bar-mode -1)               ; Disable toolbar
+(menu-bar-mode -1)               ; Disable menubar
+(scroll-bar-mode -1)             ; Disable scrollbar
+(fringe-mode 10)                 ; Add some space to the left & right border
+(setq inhibit-splash-screen t)   ; Don't show startup message
+
+;; Start emacs maximized
+(push '(fullscreen . maximized) default-frame-alist)
